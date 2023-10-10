0318d5278738d02da67a1d304e15a45a58c34e54
ButkovskiyLev <lebutkovskiy@maximum.auto>
2023-10-10 21:50:20 +0300

    tg theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import {ConfigProvider, Layout, theme} from "antd"
+import locale from "antd/lib/locale/ru_RU"
 import SPARender from "./core/components/SPARender"
+import useTelegram from "./core/hooks/useTelegram"
 import "./styles.css"
 
 function App() {
+	const {defaultAlgorithm, darkAlgorithm} = theme
+
+	const [_, options] = useTelegram()
+
 	return (
-		<div className='app'>
-			<SPARender />
-		</div>
+		<ConfigProvider
+			locale={locale}
+			theme={{
+				algorithm: options.theme === "dark" ? darkAlgorithm : defaultAlgorithm,
+			}}
+		>
+			<div className='app'>
+				<Layout style={{height: "100vh"}}>
+					<SPARender />
+				</Layout>
+			</div>
+		</ConfigProvider>
 	)
 }
 
--- src/core/hooks/useTelegram.js
+++ src/core/hooks/useTelegram.js
@@ -6,9 +6,9 @@ export default function useTelegram() {
 	useEffect(() => {
 		if (!tg) return
 		tg.ready()
-		tg.setBackgroundColor(tg.themeParams.secondary_bg_color)
-		tg.setHeaderColor(tg.themeParams.secondary_bg_color)
+		// tg.setBackgroundColor(tg.themeParams.secondary_bg_color)
+		// tg.setHeaderColor(tg.themeParams.secondary_bg_color)
 	}, [])
 
-	return [process.env.NODE_ENV === "development" ? {id: 190423552} : window?.Telegram?.WebApp?.initDataUnsafe?.user]
+	return [process.env.NODE_ENV === "development" ? {id: 190423552} : tg?.initDataUnsafe?.user, {theme: tg.colorScheme || "dark"}]
 }
